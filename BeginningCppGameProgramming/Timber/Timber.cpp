e39e7e7fc5833dbfe3ca9f9b0cacc5342c8d506d
Juan Bernardo Benavides Rubio <juan.benavides@fintegra.co>
2023-07-11 10:46:10 +0200

    Display background of game

--- BeginningCppGameProgramming/Timber/Timber.cpp
+++ BeginningCppGameProgramming/Timber/Timber.cpp
@@ -6,6 +6,15 @@ int main()
 {
     VideoMode vm(1360, 768);
     RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+    View view(sf::FloatRect(0, 0, 1920, 1080));
+    window.setView(view);
+
+    Texture textureBackground;
+    textureBackground.loadFromFile("graphics/background.png");
+
+    Sprite spriteBackground;
+    spriteBackground.setTexture(textureBackground);
+    spriteBackground.setPosition(0, 0);
 
     while (window.isOpen())
     {
@@ -33,6 +42,7 @@ int main()
         // Clear everything from the last frame
         window.clear();
         // Draw our game scene here
+        window.draw(spriteBackground);
         // Show everything we just drew
         window.display();
     }
