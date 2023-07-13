dd8068544ec3666b6831092f181cb8fdaccbffe2
Juan Bernardo Benavides Rubio <juan.benavides@fintegra.co>
2023-07-13 10:36:28 +0200

    Add other sprites to the scene

--- BeginningCppGameProgramming/Timber/main.cpp
+++ BeginningCppGameProgramming/Timber/main.cpp
@@ -9,12 +9,45 @@ int main()
     View view(sf::FloatRect(0, 0, 1920, 1080));
     window.setView(view);
 
+    // Set up background
     Texture textureBackground;
     textureBackground.loadFromFile("graphics/background.png");
 
     Sprite spriteBackground;
     spriteBackground.setTexture(textureBackground);
     spriteBackground.setPosition(0, 0);
+
+    // Set up tree
+    Texture textureTree;
+    textureTree.loadFromFile("graphics/tree.png");
+
+    Sprite spriteTree;
+    spriteTree.setTexture(textureTree);
+    spriteTree.setPosition(810, 0);
+
+    // Set up bee
+    Texture textureBee;
+    textureBee.loadFromFile("graphics/bee.png");
+
+    Sprite spriteBee;
+    spriteBee.setTexture(textureBee);
+    spriteBee.setPosition(0, 800);
+
+    // Set up clouds
+    Texture textureCloud;
+    textureCloud.loadFromFile("graphics/cloud.png");
+
+    Sprite spriteCloud1;
+    Sprite spriteCloud2;
+    Sprite spriteCloud3;
+
+    spriteCloud1.setTexture(textureCloud);
+    spriteCloud2.setTexture(textureCloud);
+    spriteCloud3.setTexture(textureCloud);
+
+    spriteCloud1.setPosition(0, 0);
+    spriteCloud2.setPosition(0, 250);
+    spriteCloud3.setPosition(0, 500);
 
     while (window.isOpen())
     {
@@ -43,6 +76,12 @@ int main()
         window.clear();
         // Draw our game scene here
         window.draw(spriteBackground);
+        window.draw(spriteCloud1);
+        window.draw(spriteCloud2);
+        window.draw(spriteCloud3);
+        window.draw(spriteTree);
+        window.draw(spriteBee);
+
         // Show everything we just drew
         window.display();
     }
